bb9ec15c53da2321c4bfc21edcf4e12d28037368
19Ishan <ishan.md1911@gmail.com>
2023-11-26 01:12:14 +0530

    Added Island
    Island Model Added
    Rotation Pending

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,30 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
+import Island from '../models/Island'
 
 const Home = () => {
+  
+    const adjustIslandForScreenSize = () => {
+      let screenScale = null;
+      let screenPosition = [0, -6.5, -43];
+      let rotation = [0.1, 4.7, 0]
+
+      if(window.innerWidth < 768) {
+        screenScale = [0.9, 0.9, 0.9];
+      } 
+
+      else {
+        screenScale = [1, 1, 1];
+      }
+
+      return [ screenScale, screenPosition];
+    }
+
+    const [islandScale, islandPosition, screenRotation] = adjustIslandForScreenSize();
+  
+
+
   return (
     <section className = "w-full h-screen relative">
       <Canvas 
@@ -15,6 +37,13 @@ const Home = () => {
           <pointLight />
           <spotLight />
           <hemisphereLight />
+
+          <Island
+            position = {islandPosition}
+            scale = {islandScale}
+            rotation = {screenRotation}
+          
+          />
         </Suspense>
       </Canvas>
     </section>
